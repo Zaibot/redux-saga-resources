308b5e3d8abe318e0ec822521a63df0e230b7108
Tobias de Groen <tdgroen@lias-software.nl>
2017-02-01 16:00:27 +0100

    wip

--- src/middlewares/json.ts
+++ src/middlewares/json.ts
@@ -4,196 +4,221 @@ import applyMiddlewares from '../utils/applyMiddlewares';
 
 
 export function authBearerMiddleware(selector) {
-    return function* internal({ request }, next) {
-        const token = yield select(selector);
-        if (token) {
-            request.headers['authorization'] = `Bearer ${token}`;
-        }
-        yield* next();
+  return function* internal({ request }, next) {
+    const token = yield select(selector);
+    if (token) {
+      request.headers['authorization'] = `Bearer ${token}`;
     }
+    yield* next();
+  }
 }
 
 export function defaultJsonRestMiddleware<T>(url, ...middlewares) {
-    return (descriptor) => {
-        return connectMiddleware(descriptor, restMiddleware({ id: descriptor.options.id, url: url }), ...middlewares, jsonSerializationMiddleware, fetchMiddleware);
-    };
+  return (descriptor) => {
+    return connectMiddleware(descriptor, restMiddleware({ id: descriptor.options.id, url: url }), ...middlewares, jsonSerializationMiddleware, fetchMiddleware);
+  };
 }
 
 import { fields, stripFields } from '../resource/fields';
 import { IResourceDescriptor } from '../resource';
 export function connectMiddleware<T>(descriptor, ...middlewares) {
-    const middleware = applyMiddlewares(...middlewares);
+  const middleware = applyMiddlewares(...middlewares);
 
-    const loadin = (item) => item ? ({ ...item, [fields.id]: item[descriptor.options.id] }) : item;
+  const loadin = (item) => item ? ({ ...item, [fields.id]: item[descriptor.options.id] }) : item;
 
-    return function* ({ action, descriptor }: { action: any, descriptor: IResourceDescriptor<any> }): IterableIterator<any> {
-        if (action.type === descriptor.actions.CREATE) {
-            const context: any = { create: stripFields(action.payload.item) };
-            yield call(middleware, context);
-            if (context.ok) {
-                yield put(descriptor.creators.doCreateSuccess({
-                    ...action.payload.item,
-                    ...context.created,
-                    [fields.id]: context.created[descriptor.options.id]
-                }));
-            } else {
-                yield put(descriptor.creators.doCreateFailure(action.payload.item, context.error));
-            }
-        }
-        if (action.type === descriptor.actions.READ) {
-            const context: any = { read: stripFields(action.payload.item) };
-            yield call(middleware, context);
-            if (context.ok) {
-                yield put(descriptor.creators.doReadSuccess({
-                    ...action.payload.item,
-                    ...context.readed
-                }));
-            } else {
-                yield put(descriptor.creators.doReadFailure(action.payload.item, context.error));
-            }
-        }
-        if (action.type === descriptor.actions.UPDATE) {
-            const context: any = { update: stripFields(action.payload.item) };
-            yield call(middleware, context);
-            if (context.ok) {
-                yield put(descriptor.creators.doUpdateSuccess({
-                    ...action.payload.item,
-                    ...context.updated
-                }));
-            } else {
-                yield put(descriptor.creators.doUpdateFailure(action.payload.item, context.error));
-            }
-        }
-        if (action.type === descriptor.actions.DELETE) {
-            const context: any = { remove: stripFields(action.payload.item) };
-            yield call(middleware, context);
-            if (context.ok) {
-                yield put(descriptor.creators.doDeleteSuccess({
-                    ...action.payload.item,
-                    ...context.deleted
-                }));
-            } else {
-                yield put(descriptor.creators.doDeleteFailure(action.payload.item, context.error));
-            }
-        }
-        if (action.type === descriptor.actions.LIST) {
-            const context: any = { list: action.payload.params || {} };
-            yield call(middleware, context);
-            if (context.ok) {
-                yield put(descriptor.creators.doListSuccess(context.listed.map(item => ({
-                    ...item,
-                    [fields.id]: item[descriptor.options.id]
-                })), action.payload.params || {}));
-            } else {
-                yield put(descriptor.creators.doListFailure(context.error, action.payload.params || {}));
-            }
-        }
-    };
+  return function* ({ action, descriptor }: { action: any, descriptor: IResourceDescriptor<any> }): IterableIterator<any> {
+    if (action.type === descriptor.actions.CREATE) {
+      // Create
+      const context: any = { create: stripFields(action.payload.item) };
+      try {
+        yield call(middleware, context);
+        if (context.ok) {
+          yield put(descriptor.creators.doCreateSuccess({
+            ...action.payload.item,
+            ...context.created,
+            [fields.id]: context.created[descriptor.options.id]
+          }));
+      } else {
+        yield put(descriptor.creators.doCreateFailure(action.payload.item, context.error));
+      }
+    } catch (ex) {
+      yield put(descriptor.creators.doCreateFailure(action.payload.item, ex));
+    }
+  }
+  if (action.type === descriptor.actions.READ) {
+    // Read
+    const context: any = { read: stripFields(action.payload.item) };
+    try {
+      yield call(middleware, context);
+      if (context.ok) {
+        yield put(descriptor.creators.doReadSuccess({
+            ...action.payload.item,
+          ...context.readed
+          }));
+    } else {
+      yield put(descriptor.creators.doReadFailure(action.payload.item, context.error));
+    }
+  } catch (ex) {
+    yield put(descriptor.creators.doReadFailure(action.payload.item, ex));
+  }
+}
+if (action.type === descriptor.actions.UPDATE) {
+  // Update
+  const context: any = { update: stripFields(action.payload.item) };
+  try {
+    yield call(middleware, context);
+    if (context.ok) {
+      yield put(descriptor.creators.doUpdateSuccess({
+            ...action.payload.item,
+        ...context.updated
+          }));
+  } else {
+    yield put(descriptor.creators.doUpdateFailure(action.payload.item, context.error));
+  }
+} catch (ex) {
+  yield put(descriptor.creators.doUpdateFailure(action.payload.item, ex));
+}
+    }
+if (action.type === descriptor.actions.DELETE) {
+  // Delete
+  const context: any = { remove: stripFields(action.payload.item) };
+  try {
+    yield call(middleware, context);
+    if (context.ok) {
+      yield put(descriptor.creators.doDeleteSuccess({
+            ...action.payload.item,
+        ...context.deleted
+          }));
+  } else {
+    yield put(descriptor.creators.doDeleteFailure(action.payload.item, context.error));
+  }
+} catch (ex) {
+  yield put(descriptor.creators.doDeleteFailure(action.payload.item, ex));
+}
+    }
+if (action.type === descriptor.actions.LIST) {
+  // List
+  const context: any = { list: action.payload.params || {} };
+  try {
+    yield call(middleware, context);
+    if (context.ok) {
+      yield put(descriptor.creators.doListSuccess(context.listed.map(item => ({
+            ...item,
+        [fields.id]: item[descriptor.options.id]
+          })), action.payload.params || {}));
+  } else {
+    yield put(descriptor.creators.doListFailure(context.error, action.payload.params || {}));
+  }
+} catch (ex) {
+  yield put(descriptor.creators.doListFailure(ex, action.payload.item));
+}
+    }
+  };
 }
 
 const getStatusOk = (...codes) => (code) => codes.indexOf(code) > -1;
 export function restMiddleware(options) {
-    const isListOk = getStatusOk(200);
-    const isReadOk = getStatusOk(200);
-    const isCreateOk = getStatusOk(200, 201);
-    const isUpdateOk = getStatusOk(200);
-    const isDeleteOk = getStatusOk(200, 404);
+  const isListOk = getStatusOk(200);
+  const isReadOk = getStatusOk(200);
+  const isCreateOk = getStatusOk(200, 201);
+  const isUpdateOk = getStatusOk(200);
+  const isDeleteOk = getStatusOk(200, 404);
 
-    return function* internal(context, next) {
-        const request = context.request = {
-            method: 'GET',
-            headers: {},
-            params: {},
-            url: `${options.url}`,
-            body: undefined
-        };
-        const response = context.response = {
-            statusCode: undefined,
-            statusText: undefined,
-            headers: {},
-            url: undefined,
-            body: undefined
-        };
+  return function* internal(context, next) {
+    const request = context.request = {
+      method: 'GET',
+      headers: {},
+      params: {},
+      url: `${options.url}`,
+      body: undefined
+    };
+    const response = context.response = {
+      statusCode: undefined,
+      statusText: undefined,
+      headers: {},
+      url: undefined,
+      body: undefined
+    };
 
-        const { list, create, read, update, remove } = context;
-        if (list) {
-            // List
-            request.method = 'GET';
-            request.url = `${options.url}`;
-            request.params = list;
-            yield* next();
-            context.ok = isListOk(response.statusCode);
-            context.listed = response.body;
-        } else if (read) {
-            // Read
-            request.method = 'GET';
-            request.url = `${options.url}/${read[options.id]}`;
-            yield* next();
-            context.ok = isReadOk(response.statusCode);
-            context.readed = response.body;
-        } else if (create) {
-            // Create
-            request.method = 'POST';
-            request.url = `${options.url}`;
-            request.body = create;
-            yield* next();
-            context.ok = isCreateOk(response.statusCode);
-            context.created = response.body;
-        } else if (update) {
-            // Update
-            request.method = 'PUT';
-            request.url = `${options.url}/${update[options.id]}`;
-            request.body = update;
-            yield* next();
-            context.ok = isUpdateOk(response.statusCode);
-            context.updated = response.body;
-        } else if (remove) {
-            // Remove
-            request.method = 'DELETE';
-            request.url = `${options.url}/${remove[options.id]}`;
-            yield* next();
-            context.ok = isDeleteOk(response.statusCode);
-            context.removed = response.body;
-        } else {
-            throw new Error('Expecting list, create, read, update or remove');
-        }
+    const { list, create, read, update, remove } = context;
+    if (list) {
+      // List
+      request.method = 'GET';
+      request.url = `${options.url}`;
+      request.params = list;
+      yield* next();
+      context.ok = isListOk(response.statusCode);
+      context.listed = response.body;
+    } else if (read) {
+      // Read
+      request.method = 'GET';
+      request.url = `${options.url}/${read[options.id]}`;
+      yield* next();
+      context.ok = isReadOk(response.statusCode);
+      context.readed = response.body;
+    } else if (create) {
+      // Create
+      request.method = 'POST';
+      request.url = `${options.url}`;
+      request.body = create;
+      yield* next();
+      context.ok = isCreateOk(response.statusCode);
+      context.created = response.body;
+    } else if (update) {
+      // Update
+      request.method = 'PUT';
+      request.url = `${options.url}/${update[options.id]}`;
+      request.body = update;
+      yield* next();
+      context.ok = isUpdateOk(response.statusCode);
+      context.updated = response.body;
+    } else if (remove) {
+      // Remove
+      request.method = 'DELETE';
+      request.url = `${options.url}/${remove[options.id]}`;
+      yield* next();
+      context.ok = isDeleteOk(response.statusCode);
+      context.removed = response.body;
+    } else {
+      throw new Error('Expecting list, create, read, update or remove');
     }
+  }
 }
 export function* fetchMiddleware({ request, response, withResponse }, next) {
-    yield* next();
+  yield* next();
 
-    const paramKeys = Object.keys(request.params);
-    const params = paramKeys.map(key => `${encodeURIComponent(key)}=${encodeURIComponent(request.params[key])}`).join('&');
-    const baseUrl = request.url;
-    const url = params ? `${baseUrl}?${params}` : baseUrl;
+  const paramKeys = Object.keys(request.params);
+  const params = paramKeys.map(key => `${encodeURIComponent(key)}=${encodeURIComponent(request.params[key])}`).join('&');
+  const baseUrl = request.url;
+  const url = params ? `${baseUrl}?${params}` : baseUrl;
 
-    const result = yield call(() => fetch(url, {
-        method: request.method,
-        headers: request.headers,
-        credentials: request.credentials,
-        body: request.body
-    })) as any;
+  const result = yield call(() => fetch(url, {
+    method: request.method,
+    headers: request.headers,
+    credentials: request.credentials,
+    body: request.body
+  })) as any;
 
-    response.statusText = result.statusText;
-    response.statusCode = result.status;
-    response.url = result.url;
-    response.headers = result.headers;
-    response.body = yield call(() => result.json());
+  response.statusText = result.statusText;
+  response.statusCode = result.status;
+  response.url = result.url;
+  response.headers = result.headers;
+  response.body = yield call(() => result.json());
 }
 
 export function* jsonSerializationMiddleware({ request, response }, next) {
   request.headers['accept'] = 'application/json';
 
-    if (request.body) {
-        request.headers['content-type'] = 'application/json'
-        request.body = JSON.stringify(request.body);
-    }
+  if (request.body) {
+    request.headers['content-type'] = 'application/json'
+    request.body = JSON.stringify(request.body);
+  }
 
-    yield* next();
+  yield* next();
 
-    if (/^application\/json/.test(response.headers['content-type'])) {
-        if (typeof response.body !== 'object') {
-            response.body = JSON.parse(response.body);
-        }
+  if (/^application\/json/.test(response.headers['content-type'])) {
+    if (typeof response.body !== 'object') {
+      response.body = JSON.parse(response.body);
     }
+  }
 }
--- src/resource/actions.ts
+++ src/resource/actions.ts
@@ -1,61 +1,61 @@
 import { internal } from '../utils/internal';
 
 export default function actions(name) {
-    return {
-        CREATE: internal(`CREATE_${name.toUpperCase()}`),
-        CREATE_CANCEL: internal(`CREATE_${name.toUpperCase()}_CANCEL`),
-        CREATE_SUCCESS: internal(`CREATE_${name.toUpperCase()}_SUCCESS`),
-        CREATE_FAILURE: internal(`CREATE_${name.toUpperCase()}_FAILURE`),
+  return {
+    CREATE: internal(`CREATE_${name.toUpperCase()}`),
+    CREATE_CANCEL: internal(`CREATE_${name.toUpperCase()}_CANCEL`),
+    CREATE_SUCCESS: internal(`CREATE_${name.toUpperCase()}_SUCCESS`),
+    CREATE_FAILURE: internal(`CREATE_${name.toUpperCase()}_FAILURE`),
 
-        READ: internal(`READ_${name.toUpperCase()}`),
-        READ_CANCEL: internal(`READ_${name.toUpperCase()}_CANCEL`),
-        READ_SUCCESS: internal(`READ_${name.toUpperCase()}_SUCCESS`),
-        READ_FAILURE: internal(`READ_${name.toUpperCase()}_FAILURE`),
+    READ: internal(`READ_${name.toUpperCase()}`),
+    READ_CANCEL: internal(`READ_${name.toUpperCase()}_CANCEL`),
+    READ_SUCCESS: internal(`READ_${name.toUpperCase()}_SUCCESS`),
+    READ_FAILURE: internal(`READ_${name.toUpperCase()}_FAILURE`),
 
-        UPDATE: internal(`UPDATE_${name.toUpperCase()}`),
-        UPDATE_CANCEL: internal(`UPDATE_${name.toUpperCase()}_CANCEL`),
-        UPDATE_SUCCESS: internal(`UPDATE_${name.toUpperCase()}_SUCCESS`),
-        UPDATE_FAILURE: internal(`UPDATE_${name.toUpperCase()}_FAILURE`),
+    UPDATE: internal(`UPDATE_${name.toUpperCase()}`),
+    UPDATE_CANCEL: internal(`UPDATE_${name.toUpperCase()}_CANCEL`),
+    UPDATE_SUCCESS: internal(`UPDATE_${name.toUpperCase()}_SUCCESS`),
+    UPDATE_FAILURE: internal(`UPDATE_${name.toUpperCase()}_FAILURE`),
 
-        DELETE: internal(`DELETE_${name.toUpperCase()}`),
-        DELETE_CANCEL: internal(`DELETE_${name.toUpperCase()}_CANCEL`),
-        DELETE_SUCCESS: internal(`DELETE_${name.toUpperCase()}_SUCCESS`),
-        DELETE_FAILURE: internal(`DELETE_${name.toUpperCase()}_FAILURE`),
+    DELETE: internal(`DELETE_${name.toUpperCase()}`),
+    DELETE_CANCEL: internal(`DELETE_${name.toUpperCase()}_CANCEL`),
+    DELETE_SUCCESS: internal(`DELETE_${name.toUpperCase()}_SUCCESS`),
+    DELETE_FAILURE: internal(`DELETE_${name.toUpperCase()}_FAILURE`),
 
-        LIST: internal(`LIST_${name.toUpperCase()}`),
-        LIST_CANCEL: internal(`LIST_${name.toUpperCase()}_CANCEL`),
-        LIST_SUCCESS: internal(`LIST_${name.toUpperCase()}_SUCCESS`),
-        LIST_FAILURE: internal(`LIST_${name.toUpperCase()}_FAILURE`),
+    LIST: internal(`LIST_${name.toUpperCase()}`),
+    LIST_CANCEL: internal(`LIST_${name.toUpperCase()}_CANCEL`),
+    LIST_SUCCESS: internal(`LIST_${name.toUpperCase()}_SUCCESS`),
+    LIST_FAILURE: internal(`LIST_${name.toUpperCase()}_FAILURE`),
 
-        RESET: internal(`RESET_${name.toUpperCase()}`),
+    RESET: internal(`RESET_${name.toUpperCase()}`),
 
-        all: [
-          internal(`CREATE_${name.toUpperCase()}`),
-          internal(`CREATE_${name.toUpperCase()}_CANCEL`),
-          internal(`CREATE_${name.toUpperCase()}_SUCCESS`),
-          internal(`CREATE_${name.toUpperCase()}_FAILURE`),
+    all: [
+      internal(`CREATE_${name.toUpperCase()}`),
+      internal(`CREATE_${name.toUpperCase()}_CANCEL`),
+      internal(`CREATE_${name.toUpperCase()}_SUCCESS`),
+      internal(`CREATE_${name.toUpperCase()}_FAILURE`),
 
-          internal(`READ_${name.toUpperCase()}`),
-          internal(`READ_${name.toUpperCase()}_CANCEL`),
-          internal(`READ_${name.toUpperCase()}_SUCCESS`),
-          internal(`READ_${name.toUpperCase()}_FAILURE`),
+      internal(`READ_${name.toUpperCase()}`),
+      internal(`READ_${name.toUpperCase()}_CANCEL`),
+      internal(`READ_${name.toUpperCase()}_SUCCESS`),
+      internal(`READ_${name.toUpperCase()}_FAILURE`),
 
-          internal(`UPDATE_${name.toUpperCase()}`),
-          internal(`UPDATE_${name.toUpperCase()}_CANCEL`),
-          internal(`UPDATE_${name.toUpperCase()}_SUCCESS`),
-          internal(`UPDATE_${name.toUpperCase()}_FAILURE`),
+      internal(`UPDATE_${name.toUpperCase()}`),
+      internal(`UPDATE_${name.toUpperCase()}_CANCEL`),
+      internal(`UPDATE_${name.toUpperCase()}_SUCCESS`),
+      internal(`UPDATE_${name.toUpperCase()}_FAILURE`),
 
-          internal(`DELETE_${name.toUpperCase()}`),
-          internal(`DELETE_${name.toUpperCase()}_CANCEL`),
-          internal(`DELETE_${name.toUpperCase()}_SUCCESS`),
-          internal(`DELETE_${name.toUpperCase()}_FAILURE`),
+      internal(`DELETE_${name.toUpperCase()}`),
+      internal(`DELETE_${name.toUpperCase()}_CANCEL`),
+      internal(`DELETE_${name.toUpperCase()}_SUCCESS`),
+      internal(`DELETE_${name.toUpperCase()}_FAILURE`),
 
-          internal(`LIST_${name.toUpperCase()}`),
-          internal(`LIST_${name.toUpperCase()}_CANCEL`),
-          internal(`LIST_${name.toUpperCase()}_SUCCESS`),
-          internal(`LIST_${name.toUpperCase()}_FAILURE`),
+      internal(`LIST_${name.toUpperCase()}`),
+      internal(`LIST_${name.toUpperCase()}_CANCEL`),
+      internal(`LIST_${name.toUpperCase()}_SUCCESS`),
+      internal(`LIST_${name.toUpperCase()}_FAILURE`),
 
-          internal(`RESET_${name.toUpperCase()}`)
-        ]
-    }
+      internal(`RESET_${name.toUpperCase()}`)
+    ]
+  }
 }
