161b47d85fc54ac6ef1400affda0fb521c80f447
Tobias de Groen <tdgroen@lias-software.nl>
2017-02-13 13:45:26 +0100

    fix(applyMiddleware): return with middleware signare and call next.

--- src/utils/applyMiddlewares.ts
+++ src/utils/applyMiddlewares.ts
@@ -1,16 +1,18 @@
 export interface Middleware<T extends Object> {
-  (param: T, next: (param: T) => IterableIterator<any>): IterableIterator<any>;
+  (param: T, next?: (param: T) => IterableIterator<any>): IterableIterator<any>;
 }
 
 
-export default function applyMiddlewares<T extends Object>(...middlewares: Middleware<T>[]): (param: T) => IterableIterator<any> {
+export default function applyMiddlewares<T extends Object>(...middlewares: Middleware<T>[]): Middleware<T> {
   return middlewares.slice(0).reverse().reduce((next: (param: T) => IterableIterator<any>, middleware: Middleware<T>) => {
     return function* (param) {
       yield* middleware(param, function* (additional) {
         yield* next(param);
       });
     };
-  }, function* (param) {
-    // console.error(`Reached end of middleware.`, param)
-  }) as (param: T) => IterableIterator<any>;
+  }, function* (param: T, next?: Middleware<T>) {
+    if (next) {
+      yield* next(param);
+    }
+  });
 }
