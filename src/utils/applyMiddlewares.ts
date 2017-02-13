10233f5ec20f73e4f890420bb40569439abcdad6
Tobias de Groen <tdgroen@lias-software.nl>
2017-02-13 14:16:22 +0100

    fix(applyMiddleware): return with middleware signare and call next.

--- src/utils/applyMiddlewares.ts
+++ src/utils/applyMiddlewares.ts
@@ -1,18 +1,21 @@
 export interface Middleware<T extends Object> {
-  (param: T, next?: (param: T) => IterableIterator<any>): IterableIterator<any>;
+  (param: T, next: (param: T) => IterableIterator<any>): IterableIterator<any>;
 }
 
 
 export default function applyMiddlewares<T extends Object>(...middlewares: Middleware<T>[]): Middleware<T> {
-  return middlewares.slice(0).reverse().reduce((next: (param: T) => IterableIterator<any>, middleware: Middleware<T>) => {
-    return function* (param) {
-      yield* middleware(param, function* (additional) {
-        yield* next(param);
-      });
-    };
-  }, function* (param: T, next?: Middleware<T>) {
-    if (next) {
-      yield* next(param);
-    }
-  });
+  const compiled = middlewares.reduceRight((next: (param: T, last: Middleware<T>) => IterableIterator<any>, current: Middleware<T>) => {
+     return function* (param, last) {
+       yield* current(param, function* (additional) {
+         yield* next(param, last);
+       });
+     };
+   }, function* (param: T, last: Middleware<T> = noop) {
+     yield* last(param, noop as any /*???*/);
+   });
+
+  return compiled;
+}
+
+function* noop<T>(param: T, next: (param: T) => IterableIterator<any>) {
 }
